247170493c55bd43c4b68e38dd768a4400174a0f
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2023-06-09 15:45:09 -0500

    changed img state rule to return empty string if key/value pair is undefined

--- server/controllers/getGalleryFromCloudinary.js
+++ server/controllers/getGalleryFromCloudinary.js
@@ -31,12 +31,13 @@ const getGalleryFromCloudinary = async (exhibit) => {
     });
 
       result.resources.forEach((item) => {
+        console.log(typeof item.context.custom.title, item.context.custom.title);
         let img = {
           url: item.url || '',
-          title: item.context ? item.context.custom.title : '',
-          size: item.context ? item.context.custom.size : '',
-          type: item.context ? item.context.custom.type : '',
-          sold: item.context ? item.context.custom.sold : '',
+          title: (item.context && item.context.custom.title) ? item.context.custom.title : '',
+          size: (item.context && item.context.custom.size) ? item.context.custom.size : '',
+          type: (item.context && item.context.custom.type) ? item.context.custom.type : '',
+          sold: (item.context && item.context.custom.sold) ? item.context.custom.sold : '',
         };
         gallery.push(img);
       });
