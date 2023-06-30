ea1995742af25ed38ff6483cd7f0274e0f28ab48
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2023-06-30 09:24:39 -0500

    contact modal styling

--- client/src/components/Email.js
+++ client/src/components/Email.js
@@ -21,27 +21,37 @@ const Email = ({ setEmailForm }) => {
   };
 
   return (
-    <form className="email" ref={emailRef} onSubmit={sendEmail} onClick={() => {setEmailForm(false)}}>
-      <div className='email_inputs grid'>
-        <div className='email_content'>
-          <label for='' className='email_label'>Name</label>
-          <input type='text' className='email_input' name='user_name'/>
+    <div className="contact" onClick={() => {setEmailForm(false)}}>
+      <div className='contact_header'>
+        <h1 className='contact_header_h1'>Contact Us</h1>
+        <p className='contact_header_p'>Please fill out the following form to contact us via email</p>
+      </div>
+      <form className="email" ref={emailRef} onSubmit={sendEmail}>
+        <div className='email_inputs_grid'>
+          <div className='email_content'>
+            <label for='' className='email_label'>Name</label>
+              <input type='text' className='email_input' name='user_name'/>
+          </div>
+          <div className='email_content'>
+            <label for='' className='email_label'>Email</label>
+            <input type='email' className='email_input' name='user_email'/>
+          </div>
+          <div className='email_content'>
+            <label for='' className='email_label'>Project</label>
+            <input type='text' className='email_input' name='user_project'/>
+          </div>
+          <div className='email_content_message'>
+            <label for='' className='email_label'>Message</label>
+            <textarea className='email_input' name='user_message'/>
+          </div>
+          <input type='submit' value='Send Message' className='email_content_button' />
         </div>
-      <div className='email_content'>
-        <label for='' className='email_label'>Email</label>
-        <input type='email' className='email_input' name='user_email'/>
+      </form>
+      <div className='contact_phone'>
+        <h2>Call Us</h2>
+        <p>1-650-888-8706</p>
       </div>
-      <div className='email_content'>
-        <label for='' className='email_label'>Project</label>
-        <input type='text' className='email_input' name='user_project'/>
-      </div>
-      <div className='email_content_message'>
-        <label for='' className='email_label'>Message</label>
-        <textarea className='email_input' name='user_message'/>
-      </div>
-      <input type='submit' value='Send Message' className='email_content_button' />
     </div>
-  </form>
   );
 
 }
