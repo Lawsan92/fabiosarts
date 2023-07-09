624aab1cd99eaa8eee8cbccf2d28bf9442a8483e
Ubuntu <ubuntu@ip-172-31-1-49.us-east-2.compute.internal>
2023-07-09 23:25:52 +0000

    added auth files for https

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const path = require('path');
 const cloudinary = require('./routes/cloudinary.js');
+const fs = require('fs');
+const https = require('https');
 
 //--------------------SERVER--------------------*/
 const app = express();
+
+//-------------------HTTPS--------------------*/
+const key = fs.readFileSync('/home/ubuntu/fabiosarts/auth/private.key');
+const cert = fs.readFileSync('/home/ubuntu/fabiosarts/auth/certificate.crt');
+const cred = {
+	key,
+	cert
+      };
+const httpsServer = https.createServer(cred, app);
+httpsServer.listen(8443, () => {console.log(`https server live on port:8443`)});
+
+// https.createServer(cred, (req, res) => {
+//   res.writeHead(200);
+//   res.end('hello world\n');
+// }).listen(8080);
 
 //--------------------MIDDLEWARE--------------------*/
 app.use(express.json());
@@ -12,6 +29,11 @@ app.use(express.static(path.join(__dirname, '../client/dist/')));
 //--------------------ROUTES--------------------*/
 app.listen(3000, () => {
   console.log('listening to http://localhost:3000');
+})
+
+app.get('/.well-known/pki-validation/A7DC6BA9ED359DF264150C3009144078.txt', (req, res, next) => {
+  res.sendFile('/home/ubuntu/fabiosarts/auth/A7DC6BA9ED359DF264150C3009144078.txt')
+  next(err);
 })
 
 app.use('/cloudinary', cloudinary);
