f080720d1624881ec43a51bc0f729bf40746bc70
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2023-07-09 20:17:16 -0500

    made paths .envs

--- server/index.js
+++ server/index.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const path = require('path');
 const cloudinary = require('./routes/cloudinary.js');
-<<<<<<< HEAD
-const fs = ('fs');
-=======
 const fs = require('fs');
 const https = require('https');
->>>>>>> refs/remotes/origin/main
+require('dotenv').config();
 
 //--------------------SERVER--------------------*/
 const app = express();
 
 //-------------------HTTPS--------------------*/
-const key = fs.readFileSync('/home/ubuntu/fabiosarts/auth/private.key');
-const cert = fs.readFileSync('/home/ubuntu/fabiosarts/auth/certificate.crt');
+const key = fs.readFileSync(process.env.KEY_PATH);
+const cert = fs.readFileSync(process.env.CERT_PATH);
 const cred = {
 	key,
 	cert
@@ -35,14 +32,9 @@ app.listen(3000, () => {
   console.log('listening to http://localhost:3000');
 })
 
-<<<<<<< HEAD
-app.get('/.well-known/pki-validation/A7DC6BA9ED359DF264150C3009144078.txt', () => {
-  res.sendFile('/Users/lawrence/fabiosarts/A7DC6BA9ED359DF264150C3009144078.txt')
-=======
 app.get('/.well-known/pki-validation/A7DC6BA9ED359DF264150C3009144078.txt', (req, res, next) => {
-  res.sendFile('/home/ubuntu/fabiosarts/auth/A7DC6BA9ED359DF264150C3009144078.txt')
+  res.sendFile(process.env.VALID_PATH)
   next(err);
->>>>>>> refs/remotes/origin/main
 })
 
 app.use('/cloudinary', cloudinary);
