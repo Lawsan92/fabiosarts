9257707f78330517f4d9c122410beca7b028f3f8
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2023-12-01 09:52:50 -0600

    added gallery header + footer componenets

---
+++ client/src/components/gallery/galleryFooter.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+const Gallery_Footer = () => {
+  return (
+    <div className='app'>
+    </div>
+  );
+}
+
+export default Gallery_Footer;
---
+++ client/src/components/gallery/galleryHeader.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+const Gallery_Header = () => {
+  return (
+    <div className='app'>
+    </div>
+  );
+}
+
+export default Gallery_Header;
