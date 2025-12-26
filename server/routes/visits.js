bde1edd72443640024506d1954220ae18eeb706c
Lawrence <lawrencesanzogni@gmail.com>
2025-12-25 23:25:24 -0600

    Addition (context swtich): Added admin privileges and HTTP authentication for requesting analytics data via password headers

--- server/routes/visits.js
+++ server/routes/visits.js
@@ -1,35 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const updateVisits = require('../controllers/updateVisits.js');
-const getVisits = require('../controllers/getVisits.js');
+const updateVisits = require("../controllers/updateVisits.js");
+const getVisits = require("../controllers/getVisits.js");
 
 //--------------------MIDDLEWARE--------------------*/
 router.use((req, res, next) => {
-  req.method === 'POST' && console.log('-POST /visits', '@', new Date);
-  req.method === 'GET' && console.log('-GET /visits', '@', new Date);
+  req.method === "POST" && console.log("-POST /visits", "@", new Date());
+  req.method === "GET" && console.log("-GET /visits", "@", new Date());
   next();
 });
 
 //--------------------ROUTES--------------------*/
 router
-  .route('/')
-  .post((req, res) => { try {
-    console.log('req.body /VISITS:', req.body)
-     if (JSON.stringify(req.body) == '{}') {
-      res.send('404, FAILED TP UPDATE \'{}\'')
-      return '404 NO DATA FOUND: \'{\''
-    };
-    updateVisits(req.body)
-    res.send('200, VISITS UPDATED')
-  } catch (error) {
-      res.send(console.error())
-  } })
-  .get((req, res) => {
+  .route("/")
+  .post((req, res) => {
     try {
-     getVisits().then((data) => {res.send({visits: data})})
+      console.log("req.body /VISITS:", req.body);
+      if (JSON.stringify(req.body) == "{}") {
+        res.send("404, FAILED TP UPDATE '{}'");
+        return "404 NO DATA FOUND: '{'";
+      }
+      updateVisits(req.body);
+      res.send("200, VISITS UPDATED");
     } catch (error) {
-      res.send(console.error())
+      res.send(console.error(error));
     }
   })
+  .get((req, res) => {
+    console.log("GET /VISITS");
+    try {
+      if (req.headers["authorization"] !== process.env.ADMIN_PASSWORD) {
+        res.send("403, UNAUTHORIZED");
+        return "403 UNAUTHORIZED";
+      } else {
+        console.log("AUTHORIZED");
+        getVisits().then((data) => {
+          res.send({ visits: data });
+        });
+      }
+    } catch (error) {
+      res.send(console.error(error));
+    }
+  });
 
 module.exports = router;
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,39 @@
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
 
 const config = (env) => {
-  console.log('env:', env);
+  console.log("env:", env);
   return {
-    mode: env['--mode'] || 'development',
+    mode: env["--mode"] || "development",
     entry: {
-      path: path.join(__dirname, 'client/src/index.js')
+      path: path.join(__dirname, "client/src/index.js"),
     },
     output: {
-      path: path.join(__dirname, 'client/dist/'),
-      filename: 'bundle.js'
+      path: path.join(__dirname, "client/dist/"),
+      filename: "bundle.js",
     },
     module: {
       rules: [
         {
           test: /\.js/,
           exclude: /nodeModules/,
-          loader: 'babel-loader'
+          loader: "babel-loader",
         },
         {
           test: /\.(css|scss)\b/,
-          use: ['style-loader', 'css-loader', 'sass-loader']
-        }
-      ]
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
     },
     devServer: {
       historyApiFallback: true,
-      static: path.join(__dirname, 'client/dist'),
+      static: path.join(__dirname, "client/dist"),
       proxy: {
-        '/': 'http://127.0.0.1:3000'
-      }
+        "/": "http://127.0.0.1:3000",
+      },
     },
-    plugins: [
-      new Dotenv({systemvars: true})
-    ]
-  }
-}
+    plugins: [new Dotenv({ systemvars: true })],
+  };
+};
 
 module.exports = config;
